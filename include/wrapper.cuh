29a1498175a92631fafed656521d67760fa08696
Arham Aneeq <arhamaneeq18@gmail.com>
2025-09-10 00:59:48 +0000

    Update type signatures in wrapper.cuh

--- include/wrapper.cuh
+++ include/wrapper.cuh
@@ -7,5 +7,5 @@ void* allocateDeviceMemory(size_t bytes);
 void  deallocateDeviceMemory(void* ptr);
 void copyMemory(void* dst, const void* src, size_t bytes, int direction);
 
-void markovStep();
-curandState* genRands();
+void markovStep(uint8_t* d_cells, int w, int h, float T, float mu, curandState* states);
+curandState* genRands(int w, int h);
